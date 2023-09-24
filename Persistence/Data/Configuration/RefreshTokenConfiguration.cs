fb5c156ed69c8ed0c81a0f55900ff862bd404e71
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-09-23 20:50:34 -0500

    feat: :art: Add New Class Users
    add Class for Authentication JWT

---
+++ Persistence/Data/Configuration/RefreshTokenConfiguration.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configuration
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable("RefreshToken");
+        }
+    }
+}
