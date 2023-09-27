9985e284fa5bd432e70706b80c97da0335434428
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-09-27 16:36:05 -0500

    feat: :sparkles: Add Class Controller Base
    Add Base Controller Project

---
+++ Apicrc/Controllers/ApiBaseController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apicrc.Controllers;
+[ApiController]
+[Route("api/crc/[controller]")]
+public class ApiBaseController : ControllerBase
+{
+
+}
