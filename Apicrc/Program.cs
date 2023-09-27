49699fdb066369a50004c263450658be4e5add7e
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-09-27 15:50:02 -0500

    feat: :sparkles: Add Control Log
    Cofigure Control Log to Project

--- Apicrc/Program.cs
+++ Apicrc/Program.cs
@@ -1,12 +1,21 @@
 using System.Reflection;
 using Apicrc.Extensions;
+using Apicrc.Helpers.Errors;
 using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+					.ReadFrom.Configuration(builder.Configuration)
+					.Enrich.FromLogContext()
+					.CreateLogger();
+
+//builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,13 +32,29 @@ builder.Services.AddDbContext<CrcdbContext>(options =>
 });
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+	var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+	try
+	{
+		var context = services.GetRequiredService<CrcdbContext>();
+		await context.Database.MigrateAsync();
+	}
+	catch (Exception ex)
+	{
+		var _logger = loggerFactory.CreateLogger<Program>();
+		_logger.LogError(ex, "Ocurrio un error durante la migracion");
+	}
+}
 app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 
